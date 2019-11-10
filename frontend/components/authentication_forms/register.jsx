74fa7d2d454a96d2ab3b521ddc3faaef4e02ad08
Akis Kourouklis <48292762+AkisKourouklis@users.noreply.github.com>
2019-11-10 18:10:16 +0200

    Update register.jsx

--- frontend/components/authentication_forms/register.jsx
+++ frontend/components/authentication_forms/register.jsx
@@ -1,54 +1,82 @@
 import { Grid, TextField, Box, Card, CardContent, CardHeader, Button, Container, FormControl } from "@material-ui/core";
+import axios from "axios";
+import React, { Component } from "react";
 
-const Register_Form = () => (
-  <div>
-    {/*Register FORM*/}
-    <Box width={1}>
-      <Card>
-        <Container>
-          {/*Card Header*/}
-          <Box mt={5}>
-            <CardHeader align="center" title="Join our network now" subheader="Let's Start" />
-          </Box>
+export default class Register_Form extends Component {
+  constructor(props) {
+    super(props);
 
-          {/*Card Content*/}
-          <CardContent>
-            {/*USERNAME & PASSWORD FIELDS*/}
-            <Box>
-              <Grid container>
-                <Grid item xs={6}>
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      password: "",
+      email: ""
+    };
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    axios.post("http://localhost:4000/api/user/add", [this.state.username, this.state.email, this.state.password]).then((window.location = "/"));
+  }
+  render() {
+    return (
+      <div>
+        {/*Register FORM*/}
+        <Box width={1}>
+          <Card>
+            <Container>
+              {/*Card Header*/}
+              <Box mt={5}>
+                <CardHeader align="center" title="Join our network now" subheader="Let's Start" />
+              </Box>
+
+              {/*Card Content*/}
+              <CardContent>
+                <form onSubmit={this.onSubmit}>
+                  {/*USERNAME & PASSWORD FIELDS*/}
+                  <Box>
+                    <Grid container>
+                      <Grid item xs={6}>
+                        <Box p={1}>
+                          <FormControl fullWidth>
+                            <TextField label="Type your username" variant="outlined" color="primary" name="username" onChange={this.onChange} />
+                          </FormControl>
+                        </Box>
+                      </Grid>
+                      <Grid item={true} xs={6}>
+                        <Box p={1}>
+                          <FormControl fullWidth>
+                            <TextField label="Type your password" variant="outlined" color="primary" type="password" name="password" onChange={this.onChange} />
+                          </FormControl>
+                        </Box>
+                      </Grid>
+                    </Grid>
+                  </Box>
+                  {/*EMAIL FIELD*/}
                   <Box p={1}>
                     <FormControl fullWidth>
-                      <TextField label="Type your username" variant="outlined" color="primary" name="username" />
+                      <TextField label="Type your email address" variant="outlined" color="primary" name="email" onChange={this.onChange} />
                     </FormControl>
                   </Box>
-                </Grid>
-                <Grid item={true} xs={6}>
-                  <Box p={1}>
-                    <FormControl fullWidth>
-                      <TextField label="Type your password" variant="outlined" color="primary" name="password" />
-                    </FormControl>
+
+                  <Box m={1} mt={10} align="right">
+                    <Button variant="contained" type="submit" color="primary">
+                      Register
+                    </Button>
                   </Box>
-                </Grid>
-              </Grid>
-            </Box>
-            {/*EMAIL FIELD*/}
-            <Box p={1}>
-              <FormControl fullWidth>
-                <TextField label="Type your email address" variant="outlined" color="primary" name="username" />
-              </FormControl>
-            </Box>
-
-            <Box m={1} mt={10} align="right">
-              <Button variant="contained" color="primary">
-                Register
-              </Button>
-            </Box>
-          </CardContent>
-        </Container>
-      </Card>
-    </Box>
-  </div>
-);
-
-export default Register_Form;
+                </form>
+              </CardContent>
+            </Container>
+          </Card>
+        </Box>
+      </div>
+    );
+  }
+}

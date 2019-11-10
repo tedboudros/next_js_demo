8e5d0e6e42d5735d34860c78538e0e37b5096742
Akis Kourouklis <48292762+AkisKourouklis@users.noreply.github.com>
2019-11-10 18:26:04 +0200

    Merge branch 'master' of https://github.com/thboud/next_js_demo

--- frontend/components/authentication_forms/register.jsx
+++ frontend/components/authentication_forms/register.jsx
@@ -1,3 +1,5 @@
+@@ -1,4 +1,14 @@
+import { Grid, TextField, Box, Card, CardContent, CardHeader, Button, Container, FormControl } from "@material-ui/core";
 import {
   Grid,
   TextField,
@@ -12,44 +14,35 @@ import {
 import axios from "axios";
 import React, { Component } from "react";
 
-export default class Register_Form extends Component {
-  constructor(props) {
+@ -7,7 +17,7 @@ export default class Register_Form extends Component {
     super(props);
 
     this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
 
     this.state = {
       username: "",
-      password: "",
-      email: ""
-    };
-  }
-
-  onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+@ -23,7 +33,13 @@ export default class Register_Form extends Component {
   }
 
   onSubmit(e) {
+    axios.post("http://localhost:4000/api/user/add", [this.state.username, this.state.email, this.state.password]).then((window.location = "/"));
     axios
-      .post("http://localhost:4000/api/user/add", [
-        this.state.username,
-        this.state.email,
-        this.state.password
-      ])
+      .post("http://localhost:4000/api/user/add", {
+        username: this.state.username,
+        email: this.state.email,
+        password: this.state.password
+      })
       .then((window.location = "/"));
   }
   render() {
     return (
-      <div>
-        {/*Register FORM*/}
-        <Box width={1}>
-          <Card>
+@ -34,7 +50,11 @@ export default class Register_Form extends Component {
             <Container>
               {/*Card Header*/}
               <Box mt={5}>
+                <CardHeader align="center" title="Join our network now" subheader="Let's Start" />
                 <CardHeader
                   align="center"
                   title="Join our network now"
@@ -58,14 +51,11 @@ export default class Register_Form extends Component {
               </Box>
 
               {/*Card Content*/}
-              <CardContent>
-                <form onSubmit={this.onSubmit}>
-                  {/*USERNAME & PASSWORD FIELDS*/}
-                  <Box>
-                    <Grid container>
+@ -46,14 +66,27 @@ export default class Register_Form extends Component {
                       <Grid item xs={6}>
                         <Box p={1}>
                           <FormControl fullWidth>
+                            <TextField label="Type your username" variant="outlined" color="primary" name="username" onChange={this.onChange} />
                             <TextField
                               label="Type your username"
                               variant="outlined"
@@ -79,6 +69,7 @@ export default class Register_Form extends Component {
                       <Grid item={true} xs={6}>
                         <Box p={1}>
                           <FormControl fullWidth>
+                            <TextField label="Type your password" variant="outlined" color="primary" type="password" name="password" onChange={this.onChange} />
                             <TextField
                               label="Type your password"
                               variant="outlined"
@@ -90,11 +81,11 @@ export default class Register_Form extends Component {
                           </FormControl>
                         </Box>
                       </Grid>
-                    </Grid>
-                  </Box>
+@ -62,7 +95,13 @@ export default class Register_Form extends Component {
                   {/*EMAIL FIELD*/}
                   <Box p={1}>
                     <FormControl fullWidth>
+                      <TextField label="Type your email address" variant="outlined" color="primary" name="email" onChange={this.onChange} />
                       <TextField
                         label="Type your email address"
                         variant="outlined"
@@ -104,18 +95,3 @@ export default class Register_Form extends Component {
                       />
                     </FormControl>
                   </Box>
-
-                  <Box m={1} mt={10} align="right">
-                    <Button variant="contained" type="submit" color="primary">
-                      Register
-                    </Button>
-                  </Box>
-                </form>
-              </CardContent>
-            </Container>
-          </Card>
-        </Box>
-      </div>
-    );
-  }
-}

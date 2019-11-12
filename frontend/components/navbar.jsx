01f5d468a39b6235bea6c64c63738ae61c762ea1
Akis Kourouklis <48292762+AkisKourouklis@users.noreply.github.com>
2019-11-12 16:00:48 +0200

    Update navbar.jsx

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -13,6 +13,19 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import initialize from "../utils/initialize";
 import actions from "../redux/actions";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px"
+  }
+});
 
 class Navbar extends Component {
   constructor(props) {
@@ -38,7 +51,11 @@ class Navbar extends Component {
               <Grid container spacing={1}>
                 <Grid item={true}>
                   <Link href="/signin" style={{ textDecoration: "none" }}>
-                    <Button variant="contained" color="third">
+                    <Button
+                      variant="contained"
+                      color="third"
+                      className={classes.root}
+                    >
                       Login
                     </Button>
                   </Link>
